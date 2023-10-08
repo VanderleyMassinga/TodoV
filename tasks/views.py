9f589989dd2c6c2fa5ec70b4ed47ac7997a8e497
VanderleyMassinga <vanderleymassinga466@gmail.com>
2023-10-08 14:30:16 +0200

    some changes

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 from .forms import *
@@ -7,6 +7,12 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
